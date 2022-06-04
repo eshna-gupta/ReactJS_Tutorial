9c148c23c6d98bbda01b25b94b89730bbbab9023
Eshna Gupta <eshna2017@gmail.com>
2022-06-04 17:14:43 +0530

    Added my first project

---
+++ Netflix/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+//ReactDOM.render( /*#__PURE__*/React.createElement("h1", null, " hello world"), document.getElementById('root'));
+ReactDOM.render(
+<>
+<h1> Eshna's Netflix picks </h1>
+<p> Here are my top 5 all time favourite Netflix movies & series!</p>
+<ol>
+   <li> Fifty shades of grey</li>
+   <li> Crazy rich Asians</li>
+   <li> You</li>
+   <li> Don't Breathe</li>
+   <li> Run</li>
+</ol>
+</>,
+document.getElementById('root'));
