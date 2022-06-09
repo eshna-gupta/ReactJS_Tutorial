ce9d141a7df3637cebffd65ab044173722a652d1
Eshna Gupta <eshna2017@gmail.com>
2022-06-10 01:52:41 +0530

    Added css file, added images and created hyperlink

--- practice/src/index.js
+++ practice/src/index.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 const name = "Eshna Gupta";
-const img1 = "https://picsum.photos/200/300"
-const img2 = " https://picsum.photos/250/300"
-const img3 = " https://picsum.photos/300/300"
+const img1 = "https://picsum.photos/200/300";
+const img2 = "https://picsum.photos/250/300";
+const img3 = "https://picsum.photos/300/300";
+const link = "https://picsum.photos/";
 ReactDOM.render(
   <>
-    <h1 contentEditable = "true" >
+  <h1 className='heading' >
     My name is {name}
-
-    </h1>
-    <img src= {img1}  alt = "Random Image" />
+  </h1>
+  <div className='img'>
+  <img src= {img1}  alt = "Random Image" />
     <img src= {img2}  alt = "Random Image" />
+    <a href= {link} target = "_blank">
     <img src= {img3}  alt = "Random Image" />
-
+    </a>
+  </div>
+    
   </>,
   document.getElementById("root")
   
