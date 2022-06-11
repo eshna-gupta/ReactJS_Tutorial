bc79f97ca1c13bc475b0b7f13153b1d070abcfb5
Eshna Gupta <eshna2017@gmail.com>
2022-06-12 01:59:40 +0530

    created inline CSS, commented out index.css file

--- practice/src/index.js
+++ practice/src/index.js
@@ -6,12 +6,24 @@ const img1 = "https://picsum.photos/200/300";
 const img2 = "https://picsum.photos/250/300";
 const img3 = "https://picsum.photos/300/300";
 const link = "https://picsum.photos/";
+
+const heading = {
+  color: ' #fa9191',
+  textAlign: 'center',
+  fontFamily: 'Indie Flower', 
+  margin: '50px'
+}
+
+const images = {
+  justifyContent: 'center',
+  display: 'flex'
+}
 ReactDOM.render(
   <>
-  <h1 className='heading' >
+  <h1 style={heading} >
     My name is {name}
   </h1>
-  <div className='img'>
+  <div style={images}>
   <img src= {img1}  alt = "Random Image" />
     <img src= {img2}  alt = "Random Image" />
     <a href= {link} target = "_blank">
